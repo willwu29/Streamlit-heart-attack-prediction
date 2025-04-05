a49826184f159346c33533357455c99c26e56510
Will Wu <willwu2912@gmail.com>
2025-04-05 14:52:43 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ col1, spacer, col2 = st.columns([1.2, 0.3, 1.2])  # Wider left column
 with col1:
     # Personal Information
     st.header("Personal Info")
-    sex = st.selectbox("Select your sex:", ["Male", "Female"])
+    sex = st.selectbox("Select your Gender:", ["Male", "Female"])
     race_ethnicity = st.selectbox("What is your race/ethnicity", 
                                 ["White", "Hispanic", "Black", "Asian", "Multiracial", "Other", "Unknown"])
     age_category = st.selectbox("Select your age category:", 

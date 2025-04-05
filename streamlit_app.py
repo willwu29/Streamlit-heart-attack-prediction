2d63bf50e16ed1a2183094698ef2efd1889d584e
Will Wu <willwu2912@gmail.com>
2025-04-05 09:40:16 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,35 +6,35 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from imblearn.pipeline import Pipeline
 
 
-###################
-# Create a title
-###################
+# ###################
+# # Create a title
+# ###################
 
 
-st.set_page_config(page_title="Heart Attack Prediction App", page_icon="💓", layout="centered")
+# st.set_page_config(page_title="Heart Attack Prediction App", page_icon="💓", layout="centered")
 
-st.markdown("<h1 style='font-size: 36px; text-align: center; color: #FF5733;'>💓🩺 Heart Attack Prediction App 🩺💓</h1>", unsafe_allow_html=True)
-st.write("<h4 style='text-align: center; color: #555;'>Use this app to predict your heart attack risk!</h4>", unsafe_allow_html=True)
+# st.markdown("<h1 style='font-size: 36px; text-align: center; color: #FF5733;'>💓🩺 Heart Attack Prediction App 🩺💓</h1>", unsafe_allow_html=True)
+# st.write("<h4 style='text-align: center; color: #555;'>Use this app to predict your heart attack risk!</h4>", unsafe_allow_html=True)
 
-# Centered message with adjusted styling
-st.markdown("<h4 style='font-size: 20px; text-align: center; color: #555;'>Please fill out your information on the left!</h4>", unsafe_allow_html=True)
+# # Centered message with adjusted styling
+# st.markdown("<h4 style='font-size: 20px; text-align: center; color: #555;'>Please fill out your information on the left!</h4>", unsafe_allow_html=True)
 
-# Introduction section
-st.markdown("<h2 style='text-align: center;'>Welcome to the Heart Attack Prediction App</h2>", unsafe_allow_html=True)
-st.markdown("""
-This application aims to assess your risk of experiencing a heart attack based on various health and lifestyle factors. 
-By filling out the information on the left, the app will use a trained machine learning model to provide a risk assessment. 
-Please ensure to provide accurate information to receive a reliable prediction.
+# # Introduction section
+# st.markdown("<h2 style='text-align: center;'>Welcome to the Heart Attack Prediction App</h2>", unsafe_allow_html=True)
+# st.markdown("""
+# This application aims to assess your risk of experiencing a heart attack based on various health and lifestyle factors. 
+# By filling out the information on the left, the app will use a trained machine learning model to provide a risk assessment. 
+# Please ensure to provide accurate information to receive a reliable prediction.
 
-### How It Works
-- The model utilizes multiple health indicators to predict heart attack risk.
-- After you fill in the required fields, click on the *Predict Heart Attack Risk* button.
-- The app will calculate the prediction and display the results along with important health guidance.
+# ### How It Works
+# - The model utilizes multiple health indicators to predict heart attack risk.
+# - After you fill in the required fields, click on the *Predict Heart Attack Risk* button.
+# - The app will calculate the prediction and display the results along with important health guidance.
 
-### Disclaimer
-This application is for informational purposes only and should not be considered a substitute for professional medical advice.
-In case of health concerns, consult a qualified healthcare professional.
-""")
+# ### Disclaimer
+# This application is for informational purposes only and should not be considered a substitute for professional medical advice.
+# In case of health concerns, consult a qualified healthcare professional.
+# """)
 
 ###################
 # Load Model
@@ -51,199 +51,96 @@ def load_model():
     except ModuleNotFoundError as e:
         st.error(f"Failed to load model due to a missing module: {str(e)}")
         st.stop()
-    
+
 model = load_model()
 
-
-
-###############################
-# Create User Input on the App
-################################
-
-
-# CSS to make the sidebar wider
-st.markdown(
-    """
-    <style>
-        .css-1d391kg {
-            width: 400px; /* Adjust the width as needed */
-        }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-
-
-# Create input fields for user input
-# sex = st.selectbox("Sex", ["Male", "Female"])
-# race_ethnicity = st.selectbox("What is your race/ethnicity", 
-#                                ["White", "Hispanic", "Black", "Asian", "Multiracial", "Other", "Unknown"])
-# age_category = st.selectbox("Age Category", 
-#                              ["18-24", "25-29", "30-34", "35-39", "40-44", "45-49", 
-#                               "50-54", "55-59", "60-64", "65-69", "70-74", "75-79", "80+"])
-# bmi_category = st.selectbox("BMI Category: (Underweight <= 18.4), (Healthy 18.5-24.9), (Overweight 25.0-29.9), (Obese >= 30.0)", 
-#                             ["Underweight", "Healthy", "Overweight", "Obese"])
-# general_health = st.selectbox("Would you say that in general your health is:", 
-#                               ["Excellent", "Very good", "Good", "Fair", "Poor", "Unknown"])
-# physical_activities = st.selectbox("In the past month, aside from your regular job, did you engage in any physical activities or exercises for exercise?", ["No", "Yes"])  
-# alcohol_drinker = st.selectbox("In the past 30 days, have you consumed at least one alcoholic drink?", 
-#                                ["No", "Yes", "Unknown"])  
-# smoker_status = st.selectbox("Please describe your smoking habit:", 
-#                              ["Never", "Former", "Some days smoker", "Every day smoker"]) 
-# deaf_or_hard_of_hearing = st.selectbox("Are you deaf or do you have serious difficulty hearing?", 
-#                                        ["No", "Yes", "Unknown"])  
-# blind_or_vision_difficulty = st.selectbox("Are you blind or do you have serious difficulty seeing, even when wearing glasses?", 
-#                                           ["No", "Yes", "Unknown"])  
-# difficulty_walking = st.selectbox("Do you have serious difficulty walking or climbing stairs?", 
-#                                   ["No", "Yes", "Unknown"])  
-# difficulty_dressing_bathing = st.selectbox("Do you have difficulty dressing or bathing?", 
-#                                            ["No", "Yes", "Unknown"])  
-# had_depressive_disorder = st.selectbox("Have you ever been diagnosed with Depressive Disorder?", 
-#                                        ["No", "Yes", "Unknown"]) 
-# had_diabetes = st.selectbox("Have you ever been diagnosed with Diabetes?", 
-#                             ["No", "Yes", "Pre-diabetes", "Gestational-diabetes", "Unknown"])  
-# had_kidney_disease = st.selectbox("Have you ever been diagnosed with Kidney Disease?", 
-#                                   ["No", "Yes", "Unknown"])  
-# had_angina = st.selectbox("Have you ever been diagnosed with Angina, which is a type of chest pain caused by reduced blood flow to the heart?", 
-#                           ["No", "Yes"])  
-# had_stroke = st.selectbox("Have you ever had a Stroke, which is a medical condition where the blood supply to the brain is interrupted or reduced?", 
-#                           ["No", "Yes"])  
-# had_copd = st.selectbox("Have you ever been diagnosed with Chronic Obstructive Pulmonary Disease (COPD), which is a progressive lung disease that makes it difficult to breathe due to airflow blockage?", 
-#                         ["No", "Yes", "Unknown"])  
-# had_arthritis = st.selectbox("Have you ever been diagnosed with Arthritis?", 
-#                              ["No", "Yes", "Unknown"])  
+# Sidebar for user input organized into sections
 with st.sidebar:
-    st.markdown("### Gender")
-    sex = st.selectbox("Select your gender:",["Male", "Female"])
-    
-    st.markdown("### Race/Ethnicity")
+    st.markdown("<h3 style='font-size: 24px;'>Personal Information</h3>", unsafe_allow_html=True)
+    sex = st.selectbox("Select your sex:", ["Male", "Female"])
     race_ethnicity = st.selectbox("What is your race/ethnicity", 
                                    ["White", "Hispanic", "Black", "Asian", "Multiracial", "Other", "Unknown"])
-    
-    st.markdown("### Age Category")
     age_category = st.selectbox("Select your age category:", 
                                  ["18-24", "25-29", "30-34", "35-39", "40-44", "45-49", 
                                   "50-54", "55-59", "60-64", "65-69", "70-74", "75-79", "80+"])
-    
-    st.markdown("### BMI Category")
+
+    st.markdown("<h3 style='font-size: 24px;'>Habits & Lifestyle</h3>", unsafe_allow_html=True)
     bmi_category = st.selectbox("BMI Category: (Underweight <= 18.4), (Healthy 18.5-24.9), (Overweight 25.0-29.9), (Obese >= 30.0)", 
                                 ["Underweight", "Healthy", "Overweight", "Obese"])
-    
-    st.markdown("### General Health")
     general_health = st.selectbox("Would you say that in general your health is:", 
                                   ["Excellent", "Very good", "Good", "Fair", "Poor", "Unknown"])
-    
-    st.markdown("### Physical Activities")
     physical_activities = st.selectbox("In the past month, did you engage in any physical activities or exercises?", ["No", "Yes"])  
-    
-    st.markdown("### Alcohol Drinker")
     alcohol_drinker = st.selectbox("In the past 30 days, have you consumed at least one alcoholic drink?", 
                                    ["No", "Yes", "Unknown"])  
-    
-    st.markdown("### Smoking Habit")
     smoker_status = st.selectbox("Please describe your smoking habit:", 
                                  ["Never", "Former", "Every day smoker", "Some days smoker"])
-    
-    st.markdown("### Hearing Difficulty")
-    deaf_or_hard_of_hearing = st.selectbox("Do you have serious difficulty hearing?", ["No", "Yes", "Unknown"])
-    
-    st.markdown("### Vision Difficulty")
-    blind_or_vision_difficulty = st.selectbox("Do you have serious difficulty seeing, even when wearing glasses?", ["No", "Yes", "Unknown"])
-    
-    st.markdown("### Difficulty Walking")
-    difficulty_walking = st.selectbox("Do you have serious difficulty walking or climbing stairs?", ["No", "Yes", "Unknown"])
-    
-    st.markdown("### Difficulty Dressing/Bathing")
-    difficulty_dressing_bathing = st.selectbox("Do you have difficulty dressing or bathing?", ["No", "Yes", "Unknown"])
-    
-    st.markdown("### Depressive Disorder")
-    had_depressive_disorder = st.selectbox("Have you ever been diagnosed with Depressive Disorder?", ["No", "Yes", "Unknown"])
-    
-    st.markdown("### Diabetes")
-    had_diabetes = st.selectbox("Have you ever been diagnosed with Diabetes?", ["No", "Yes", "Pre-diabetes", "Gestational-diabetes", "Unknown"])
 
-    st.markdown("### Kidney Disease")
-    had_kidney_disease = st.selectbox("Have you ever been diagnosed with Kidney Disease?", ["No", "Yes", "Unknown"])
-    
-    st.markdown("### Angina")
-    had_angina = st.selectbox("Have you ever been diagnosed with Angina?", ["No", "Yes"])
-    
-    st.markdown("### Stroke")
-    had_stroke = st.selectbox("Have you ever had a Stroke?", ["No", "Yes"])
-    
-    st.markdown("### Chronic Obstructive Pulmonary Disease (COPD)")
-    had_copd = st.selectbox("Have you ever been diagnosed with COPD?", ["No", "Yes", "Unknown"])
-
-    st.markdown("### Arthritis")
+    st.markdown("<h3 style='font-size: 24px;'>Medical History</h3>", unsafe_allow_html=True)
+    deaf_or_hard_of_hearing = st.selectbox("Do you have serious difficulty hearing?", ["No", "Yes", "Unknown"])  
+    blind_or_vision_difficulty = st.selectbox("Do you have serious difficulty seeing, even when wearing glasses?", ["No", "Yes", "Unknown"])  
+    difficulty_walking = st.selectbox("Do you have serious difficulty walking or climbing stairs?", ["No", "Yes", "Unknown"])  
+    difficulty_dressing_bathing = st.selectbox("Do you have difficulty dressing or bathing?", ["No", "Yes", "Unknown"])  
+    had_depressive_disorder = st.selectbox("Have you ever been diagnosed with Depressive Disorder?", ["No", "Yes", "Unknown"]) 
+    had_diabetes = st.selectbox("Have you ever been diagnosed with Diabetes?", ["No", "Yes", "Pre-diabetes", "Gestational-diabetes", "Unknown"])  
+    had_kidney_disease = st.selectbox("Have you ever been diagnosed with Kidney Disease?", ["No", "Yes", "Unknown"])  
+    had_angina = st.selectbox("Have you ever been diagnosed with Angina?", ["No", "Yes"])  
+    had_stroke = st.selectbox("Have you ever had a Stroke?", ["No", "Yes"])  
+    had_copd = st.selectbox("Have you ever been diagnosed with Chronic Obstructive Pulmonary Disease (COPD)?", ["No", "Yes", "Unknown"])  
     had_arthritis = st.selectbox("Have you ever been diagnosed with Arthritis?", ["No", "Yes", "Unknown"])
 
-    
 # Prepare input data
 input_data = [
-    sex,                                # Gender
-    race_ethnicity,                     # race_ethnicity_category
-    age_category,                       # age_category
-    bmi_category.lower(),               # bmi_category
-    alcohol_drinker,                    # alcohol_drinkers
-    general_health,                     # general_health
-    smoker_status,                      # smoker_status
-    physical_activities,                # Physical activities
-    had_angina,                         # had_angina
-    had_stroke,                         # had_stroke
-    had_copd,                           # had_copd
-    had_diabetes,                       # had_diabetes
-    had_kidney_disease,                 # had_kidney_disease
-    had_depressive_disorder,            # had_depressive_disorder
-    had_arthritis,                      # had_arthritis
-    deaf_or_hard_of_hearing,            # deaf_or_hard_of_hearing
-    blind_or_vision_difficulty,         # blind_or_vision_difficulty
-    difficulty_walking,                 # difficulty_walking
-    difficulty_dressing_bathing         # difficulty_dressing_bathing
+    1 if sex == "Female" else 0,  # Gender
+    race_ethnicity,                # Race/Ethnicity
+    age_category,                  # Age Category
+    bmi_category.lower(),          # BMI Category
+    alcohol_drinker,               # Alcohol Drinkers
+    general_health.lower(),        # Convert to lowercase
+    smoker_status.lower(),         # Convert to lowercase
+    1 if physical_activities == "Yes" else 0,  # Physical Activities
+    1 if had_angina == "Yes" else 0,  # Had Angina
+    1 if had_stroke == "Yes" else 0,  # Had Stroke
+    1 if had_copd == "Yes" else 0,    # Had COPD
+    had_diabetes.lower(),           # Convert to lowercase
+    had_kidney_disease.lower(),     # Convert to lowercase
+    had_depressive_disorder.lower(), # Convert to lowercase
+    had_arthritis.lower(),           # Convert to lowercase
+    1 if deaf_or_hard_of_hearing == "Yes" else 0,  # Hearing Difficulty
+    1 if blind_or_vision_difficulty == "Yes" else 0,  # Vision Difficulty
+    1 if difficulty_walking == "Yes" else 0,      # Difficulty Walking
+    1 if difficulty_dressing_bathing == "Yes" else 0  # Difficulty Dressing/Bathing
 ]
 
-# Create input column name that match the model inpul column name and order
+# Create input column names that match the model input column name and order
 input_columns = [
-       'sex', 'race_ethnicity_category', 'age_category', 'bmi_category',
-       'alcohol_drinkers', 'general_health', 'smoker_status',
-       'physical_activities', 'had_angina', 'had_stroke', 'had_copd',
-       'had_diabetes', 'had_kidney_disease', 'had_depressive_disorder',
-       'had_arthritis', 'deaf_or_hard_of_hearing',
-       'blind_or_vision_difficulty', 'difficulty_walking',
-       'difficulty_dressing_bathing']
+    'is_female', 'race_ethnicity_category', 'age_category', 'bmi_category',
+    'alcohol_drinkers', 'general_health', 'smoker_status',
+    'physical_activities', 'had_angina', 'had_stroke', 'had_copd',
+    'had_diabetes', 'had_kidney_disease', 'had_depressive_disorder',
+    'had_arthritis', 'deaf_or_hard_of_hearing',
+    'blind_or_vision_difficulty', 'difficulty_walking',
+    'difficulty_dressing_bathing'
+]
 
-# Creae a input df
+# Create input DataFrame
 input_df = pd.DataFrame([input_data], columns=input_columns)
-
 
 ##################
 # Make Prediction
 ##################
-# Get prediction probability and apply custom threshold
 if st.button('Predict Heart Attack Risk'):
     try:
-        # Get probability of positive class
-        proba = model.predict_proba(input_df)[0][1]
+        proba = model.predict_proba(input_df)[0][1]  # Get probability of the positive class
+        threshold = 0.5  # Define a threshold for classification
+
+        prediction = 'High Risk' if proba >= threshold else 'Low Risk'
         
-        # Retrieve threshold from the model
-        model_threshold = model.named_steps['logreg'].threshold
-        
-        # Make prediction using custom threshold
-        prediction = 'High Risk' if proba >= model_threshold else 'Low Risk'
-        
-        # Display results
         st.subheader('Results')
-        
-        # Add interpretation
         if prediction == 'High Risk':
-            st.error("⚠️ Warning! Our assessment indicates you are at high risk for a heart attack. " \
-            "It is crucial that you consult a healthcare professional immediately for further evaluation.")
+            st.error("⚠️ Warning! Our assessment indicates you are at high risk for a heart attack. " 
+                      "It is crucial that you consult a healthcare professional immediately for further evaluation.")
         else:
             st.success("✅ Good News! Our assessment indicates you are at low risk for a heart attack. Keep up the good work and maintain a healthy lifestyle!")
             
     except Exception as e:
-        st.error(f"An error occurred: {str(e)}")
-        st.write("Please check your input values and try again.")
-
-# Create an input DataFrame
-input_df = pd.DataFrame([input_data], columns=input_columns)
+        st.error(f"An error occurred while making the prediction: {str(e)}")

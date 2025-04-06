bd758882473b3e333c4ed04dd7abb29d070fc2df
Will Wu <willwu2912@gmail.com>
2025-04-06 11:50:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -180,6 +180,11 @@ elif st.session_state.page == 'predict':
     input_df = pd.DataFrame([input_data], columns=input_columns)
 
     # Prediction logic
+    st.markdown("<div style='background-color: #f8f9fa; padding: 20px; border-radius: 10px; text-align: center;'>", unsafe_allow_html=True)
+    if st.button('Predict Heart Attack Risk'):
+        try:
+            threshold = model.named_steps['logreg'].threshold   # Retrieve the threshold
+            proba = model.predict_proba(input_df)[0][1]    # Make soft predictions
     if st.button('Predict Heart Attack Risk'):
         try:
             threshold = model.named_steps['logreg'].threshold   # Retreieve the threshold

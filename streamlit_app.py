f9c7f99a8175303422445300968884f275ca972e
Will Wu <willwu2912@gmail.com>
2025-04-07 12:03:49 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,8 +11,8 @@ from imblearn.pipeline import Pipeline
 st.markdown("""
 <style>
     [data-testid="stSidebar"] {
-        min-width: 250px !important;  # Increased width
-        max-width: 250px !important;  # Increased width
+        min-width: 150px !important;  # Increased width
+        max-width: 150px !important;  # Increased width
     }
     .stButton>button {
         width: 100%;

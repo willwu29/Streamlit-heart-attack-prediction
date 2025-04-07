cbc1c418635d7d226df41af4ce50294e2d62a141
Will Wu <willwu2912@gmail.com>
2025-04-07 01:00:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -424,7 +424,7 @@ elif st.session_state.page == 'eda':
     st.markdown("#### Heart Attack Occurrence Distribution")
     try:
         st.image("src/Heart_Attack_Occurrence_Distribution.png", 
-                 width=900)
+                 width=600)
     except FileNotFoundError:
         st.error("Age group distribution image not found at src/heart_attack_age_group.png")
 
@@ -432,7 +432,7 @@ elif st.session_state.page == 'eda':
     st.markdown("#### Age Group Distribution of Heart Attacks")
     try:
         st.image("src/heart_attack_age_group.png", 
-                 width=900)  # Increased from 600 to 900 pixels
+                 width=1200)  # Increased from 600 to 900 pixels
     except FileNotFoundError:
         st.error("Age group distribution image not found at src/heart_attack_age_group.png")
 
